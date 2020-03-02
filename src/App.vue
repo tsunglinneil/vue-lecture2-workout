d117fdc2c54df029fb698ccd332ee166fd912ecd
tsunglinneil <magic3657@gmail.com>
2020-03-03 07:53:15 +0800

    finish workout2

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
 <template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Filters</h1>
+        <!-- 1. 建立一個儲存字串的資料變數，利用filters計算字串長度，並顯示如: test (4) -->
+        <p></p>
 
+        <!-- 2. 實現上述功能但是這次使用computed實現 -->
+        <p></p>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  filters: {},
+  computed: {},
+};
+</script>
 
-<style lang="scss"></style>
+<style></style>
