5dc7e8349869663f62557c1c78dcd96a2c584276
tsunglinneil <magic3657@gmail.com>
2020-03-03 08:07:14 +0800

    finish workout3

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,38 @@
 <template>
-
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Filters</h1>
+        <h4>By Filters</h4>
+        <p>{{ greeting | countLengthFilters }}</p>
+        <hr />
+        <!-- 1. 使用Mixins的方式共享computed properties -->
+        <h4>By Computed</h4>
+        <p>{{ countLengthComputed }}</p>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  mixins: [],
+  data() {
+    return {
+      greeting: "Hello",
+    };
+  },
+  filters: {
+    countLengthFilters(value) {
+      return `${value} (${value.length})`;
+    },
+  },
+  computed: {
+    countLengthComputed() {
+      return `${this.greeting} (${this.greeting.length})`;
+    },
+  },
+};
+</script>
 
-<style lang="scss"></style>
+<style></style>
