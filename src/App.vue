8d2f78b7072d721a6dba9f357d13bbb82c928e35
tsunglinneil <magic3657@gmail.com>
2020-03-04 16:08:01 +0800

    finish workout2

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,10 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Filters</h1>
         <!-- 1. 建立一個儲存字串的資料變數，利用filters計算字串長度，並顯示如: test (4) -->
-        <p></p>
+        <p>Filter: {{ message | getDescibeByFilter }}</p>
 
         <!-- 2. 實現上述功能但是這次使用computed實現 -->
-        <p></p>
+        <p>Computed: {{ getDescibeByComputed }}</p>
       </div>
     </div>
   </div>
@@ -16,10 +16,20 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      message: "test",
+    };
   },
-  filters: {},
-  computed: {},
+  filters: {
+    getDescibeByFilter(value) {
+      return `${value} (${value.length})`;
+    },
+  },
+  computed: {
+    getDescibeByComputed() {
+      return `${this.message} (${this.message.length})`;
+    },
+  },
 };
 </script>
 
